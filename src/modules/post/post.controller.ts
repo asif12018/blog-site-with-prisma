81ae0502628237219ab73fb283f55534df73e804
asif12018 <asif.request@gmail.com>
2026-01-10 22:58:48 +0600

    search by authorid and featured post added

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,63 +1,80 @@
 import { Request, Response } from "express";
 import { PostService } from "./post.service";
+import { PostStatus } from "../../../generated/prisma/enums";
 
 
-
-const createPost = async(req:Request, res:Response) =>{
-    try{
-       const data = req.body;
-       if(data.length === 0){
-         return res.status(404).json({
-            success: false,
-            message:"missing data on req.body"
-         })
-       }
-       const user = req.user
-       if(!user){
-          return res.status(400).json({
-            error: "Unauthorized"
-          })
-       }
-       const result = await PostService.createPost(data, user.id as string);
-       return res.status(201).json({
-        success: true,
-        message: "post created successfully",
-        data: result
-       })
-    }catch(err: any){
-        res.status(500).json({
-            success:false,
-            message: err.message,
-            details: err
-        })
+const createPost = async (req: Request, res: Response) => {
+  try {
+    const data = req.body;
+    if (data.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "missing data on req.body",
+      });
     }
-}
-
+    const user = req.user;
+    if (!user) {
+      return res.status(400).json({
+        error: "Unauthorized",
+      });
+    }
+    const result = await PostService.createPost(data, user.id as string);
+    return res.status(201).json({
+      success: true,
+      message: "post created successfully",
+      data: result,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+      details: err,
+    });
+  }
+};
 
 //get post controller
 
-const getAllPost = async(req:Request, res:Response)=>{
-  try{
-      const {search} = req.query;
-      const searchString = typeof search === 'string' ? search : undefined;
+const getAllPost = async (req: Request, res: Response) => {
+  try {
+    const { search } = req.query;
+    const searchString = typeof search === "string" ? search : undefined;
 
-      const tags = req.query.tags ? (req.query.tags as string).split(",") : [];
-      
-      const result = await PostService.getAllPost({search: searchString, tags});
-      return res.status(200).json({
-        success: true,
-        message: "post retrieved",
-        data: result
-      })
-  }catch(err:any){
+    // accepting true and false
+    const isFeatured =
+      req.query.isFeatured === "true"
+        ? true
+        : req.query.isFeatured === "false"
+        ? false
+        : undefined;
+
+    const tags = req.query.tags ? (req.query.tags as string).split(",") : [];
+
+    const status = req.query.status as PostStatus | undefined;
+
+    const authorId = req.query.authorId as string | undefined;
+
+    const result = await PostService.getAllPost({
+      search: searchString,
+      tags,
+      isFeatured,
+      status,
+      authorId 
+    });
+    return res.status(200).json({
+      success: true,
+      message: "post retrieved",
+      data: result,
+    });
+  } catch (err: any) {
     return res.status(500).json({
-      success:false,
+      success: false,
       message: err.message,
-      details: err
-    })
+      details: err,
+    });
   }
-}
+};
 export const PostController = {
-    createPost,
-    getAllPost
-}
+  createPost,
+  getAllPost,
+};
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,4 +1,4 @@
-import { Post, Prisma } from "../../../generated/prisma/client";
+import { Post, PostStatus, Prisma } from "../../../generated/prisma/client";
 import { prisma } from "../../lib/prisma";
 
 //create a post service
@@ -22,45 +22,60 @@ const createPost = async (
 const getAllPost = async (payload: {
   search: string | undefined;
   tags: string[] | [];
+  isFeatured: boolean | undefined;
+  status: PostStatus | undefined;
+  authorId: string | undefined;
 }) => {
-
-  const andConditions:Prisma.PostWhereInput[] = [];
-  if(payload.search){
-     andConditions.push({
-          OR: [
-            {
-              title: {
-                contains: payload.search ,
-                mode: "insensitive",
-              },
-            },
-            {
-              content: {
-                contains: payload.search ,
-                mode: "insensitive",
-              },
-            },
-            {
-              //this is array. so contains will not work
-              tags: {
-                has: payload.search ,
-              },
-            },
-          ],
-        })
-  }
-
-  if(payload.tags.length > 0){
-      andConditions.push({
-          tags: {
-            hasEvery: payload.tags ,
+  const andConditions: Prisma.PostWhereInput[] = [];
+  if (payload.search) {
+    andConditions.push({
+      OR: [
+        {
+          title: {
+            contains: payload.search,
+            mode: "insensitive",
           },
-        })
+        },
+        {
+          content: {
+            contains: payload.search,
+            mode: "insensitive",
+          },
+        },
+        {
+          //this is array. so contains will not work
+          tags: {
+            has: payload.search,
+          },
+        },
+      ],
+    });
   }
+
+  if (payload.tags.length > 0) {
+    andConditions.push({
+      tags: {
+        hasEvery: payload.tags,
+      },
+    });
+  }
+
+  if (typeof payload.isFeatured === "boolean") {
+    andConditions.push({ isFeatured: payload.isFeatured });
+  }
+
+  if (payload.status) {
+    andConditions.push({ status: payload.status });
+  }
+
+  if (payload.authorId){
+    andConditions.push({authorId: payload.authorId})
+  }
+
   const result = await prisma.post.findMany({
     where: {
       //and logic. if search not available it will skip the search and go for tags
-      AND: andConditions
+      AND: andConditions,
     },
   });
   return result;
